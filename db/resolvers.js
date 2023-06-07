67fce70e98981728fdda638971bdb7e171455a1a
jrodva <jr.rodriguez@crowdfarming.com>
2023-06-07 21:52:20 +0200

    feat: add product schema

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: '.env.local'});
 
 const User = require("../models/User");
+const Product = require("../models/Product");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
